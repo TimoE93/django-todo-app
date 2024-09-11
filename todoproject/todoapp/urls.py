7f747560865003663bd973cf869c49eb51e81924
Engler Timo <timo.engler@baugruppe-gross.de>
2024-09-11 13:31:07 +0200

    commit

--- todoproject/todoapp/urls.py
+++ todoproject/todoapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "todoapp"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:todo_id>/", views.detail, name="detail")
--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -1,14 +1,24 @@
 import re
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+
 from .models import Todo
+from .forms import TodoForm
 
 def index(request):
     todos = Todo.objects.all()
     output = ", ".join([todo.title for todo in todos])
-    return HttpResponse(output)
+    context = {
+        "todos": todos
+    }
+
+    return render(request, "todoapp/index.html", context)
 
 
 def detail(request, todo_id):
-    response = "Todo %s"
-    return HttpResponse(response % todo_id)
+    import pdb; pdb.set_trace()
+    todo = get_object_or_404(Todo, pk=todo_id)
+    form = TodoForm(instance=todo)
+    
+    
+    return render(request, "todoapp/detail.html", {"todo": todo, "form": form})
